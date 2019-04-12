616325b45c4d74008ba791b956dde75db0cb0dd2
Kyossen <44588678+Kyossen@users.noreply.github.com>
2019-04-12 17:15:17 +0200

    Add files
    Add files

---
+++ Project_5/import_categories.py
@@ -0,0 +1,24 @@
+import requests
+import os
+import sys
+import django
+from django.core.paginator import Paginator
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Project_5.settings")
+django.setup()
+from purebeurre.models import Category
+
+resultat = requests.get("https://fr.openfoodfacts.org/categories.json")
+response = resultat.json()
+
+for search_category in response:
+    i = 0
+    for i in range(0, 13822):
+        i += 1
+
+        categories = Category(name=response["tags"][i]["name"], id_off=response["tags"][i]["products"],
+                        url=response["tags"][i]["url"])
+        categories.save()
+        print("Name: ", response["tags"][i]["name"], '\n',
+              "Url: ", response["tags"][i]["url"], '\n',
+              )
