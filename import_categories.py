376d0a3b8a4976b371f21c89bd6809f54795f0b9
Kyossen <44588678+Kyossen@users.noreply.github.com>
2019-04-23 13:59:46 +0200

    Add
    Add

---
+++ import_categories.py
@@ -0,0 +1,42 @@
+# usr/env/bin Python3.4
+# coding:utf-8
+
+"""
+Import these modules for a good use of the program
+Requests module for that the can program "requests to servers"
+Django for requests handler between servers and code, database, etc...
+And others modules such "os" for good use
+"""
+
+# Import lib
+import os
+import sys
+
+# Import module
+import requests
+
+# Import Django
+import django
+from django.core.paginator import Paginator
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Project_5.settings")
+django.setup()
+
+# Import file
+from purebeurre.models import Category
+
+"""This code is for save in database the totality of the categories in API to use program correctly"""
+result = requests.get("https://fr.openfoodfacts.org/categories.json")
+response = result.json()
+
+for search_category in response:
+    i = 0
+    for i in range(0, 13822):
+        i += 1
+
+        categories = Category(name=response["tags"][i]["name"], id_off=response["tags"][i]["products"],
+                        url=response["tags"][i]["url"])
+        categories.save()
+        print("Name: ", response["tags"][i]["name"], '\n',
+              "Url: ", response["tags"][i]["url"], '\n',
+              )
